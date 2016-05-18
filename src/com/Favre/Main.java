c1a3e27979be9cd2b98cd257a57dd241f2eb2055
Liz Favre <egfavre@gmail.com>
2016-05-18 11:31:45 -0400

    2016May18 Start TextAdventure

--- src/com/Favre/Main.java
+++ src/com/Favre/Main.java
@@ -1,8 +1,43 @@
 package com.Favre;
 
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws Exception {
+	    System.out.println("Welcome traveller!");
+        System.out.println("What is your name?");
+
+        Scanner scanner = new Scanner(System.in);
+
+        String name = scanner.nextLine();
+        System.out.println("Welcome, " + name);
+
+        System.out.println("Choose your weapon [sword/mace]");
+        String weapon = scanner.nextLine();
+
+        if(weapon.equalsIgnoreCase("sword")){
+            System.out.println("sword is a fine choice.");
+        }
+        else if(weapon.equalsIgnoreCase("mace")){
+            System.out.println("Mace is a fine choice.");
+        }
+        else{
+            throw new Exception("Invalid weapon");
+        }
+
+        System.out.println("Choose your location [forest/tunnel]");
+        String location = scanner.nextLine();
+
+        if (location.equalsIgnoreCase("forest")){
+            System.out.println("Entering Forest...");
+        }
+        else if (location.equalsIgnoreCase("tunnel")){
+            System.out.println("Entering tunnel...");
+        }
+        else{
+            throw new Exception("Invalid location");
+        }
+
     }
 }
