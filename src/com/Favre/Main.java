42c3cabb879112288cd841882c3c7b012a9ba0d6
Liz Favre <egfavre@gmail.com>
2016-05-25 11:30:32 -0400

    allow start new game

--- src/com/Favre/Main.java
+++ src/com/Favre/Main.java
@@ -1,8 +1,10 @@
 package com.Favre;
 
+import jodd.json.JsonParser;
 import jodd.json.JsonSerializer;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
@@ -10,26 +12,47 @@ import java.util.Scanner;
 //pull in a library
 
 public class Main {
-
+    static final String SAVE_FILE = "game.json";
     static Scanner scanner = new Scanner(System.in);
-    static Player player = new Player();
+    static Player player;
 
 
     public static void main(String[] args) throws Exception {
+        player = loadGame();
+        if (player == null) {
+            player = new Player();
+            System.out.println("Starting new game.");
+        }
+        else {
+            System.out.println("Found saved file.");
+            System.out.println("Start new game instead? [y/n");
+            String answer = scanner.nextLine();
+            if (answer.equalsIgnoreCase("y"))
+            {player = new Player();
+        }
+
         System.out.println("Welcome traveller!");
 
 
-        player.chooseName();
-        player.chooseWeapon();
-        player.chooseLocation();
+        if (player.name == null) {
+            player.chooseName();
+        }
+        if (player.weapon == null) {
+            player.chooseWeapon();
+        }
+        if (player.location == null) {
+            player.chooseLocation();
+        }
 
-        player.fintItem("armor");
-        player.fintItem("potion");
+        if (player.items.isEmpty()) {
+            player.fintItem("armor");
+            player.fintItem("potion");
+        }
 
         Enemy ogre = new Enemy("Ogre", 10, 10);
         player.battle(ogre);
 
-        //saveGame();
+        saveGame();
 
 
     }
@@ -60,7 +83,7 @@ public class Main {
         //String json = serializer.serialize(player);
         String json = serializer.include("*").serialize(player);
 
-        File f = new File("game.json");
+        File f = new File(SAVE_FILE);
         try {
             FileWriter fw = new FileWriter(f);
             fw.write(json);
@@ -70,6 +93,21 @@ public class Main {
         }
     }
 
+    public static Player loadGame() {
+        File f = new File(SAVE_FILE);
+        try {
+            Scanner scanner = new Scanner(f);
+            scanner.useDelimiter("\\Z");
+            String contents = scanner.next();
+            JsonParser parser = new JsonParser();
+            return parser.parse(contents, Player.class);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
+
+
+    }
 
 //        System.out.println("type a number...");
 //        String num = scanner.nextLine();
